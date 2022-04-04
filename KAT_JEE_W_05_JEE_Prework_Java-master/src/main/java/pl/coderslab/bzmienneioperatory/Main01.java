3594058ebcee8a7afba1b9e720260904b53f1175
KaCper1997 <chmiel-97@o2.pl>
2022-04-04 22:58:36 +0200

    Zmienne i operaotry prework

--- KAT_JEE_W_05_JEE_Prework_Java-master/src/main/java/pl/coderslab/bzmienneioperatory/Main01.java
+++ KAT_JEE_W_05_JEE_Prework_Java-master/src/main/java/pl/coderslab/bzmienneioperatory/Main01.java
@@ -3,6 +3,16 @@ package pl.coderslab.bzmienneioperatory;
 public class Main01 {
 
 	public static void main(String[] args) {
+		int number1 = 1;
+		System.out.println(number1);
+		char litera2 = 'd';
+		System.out.println(litera2);
+		long number3 = 2313124;
+		System.out.println(number3);
+		double number4 = 23.12;
+		System.out.println(number4);
+		boolean number5 = number1>number3;
+		System.out.println(number5);
 
 	}
 
