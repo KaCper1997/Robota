88fe44c9ca132ccee9ce0d2220170d8ef8eda098
KaCper1997 <chmiel-97@o2.pl>
2022-04-04 19:17:23 +0200

    Import projektu i testowanie

--- KAT_JEE_W_05_JEE_Prework_Java-master/src/main/java/pl/coderslab/apierwszyprogram/Main03.java
+++ KAT_JEE_W_05_JEE_Prework_Java-master/src/main/java/pl/coderslab/apierwszyprogram/Main03.java
@@ -3,8 +3,8 @@ package pl.coderslab.apierwszyprogram;
 public class Main03 {
 
 	public static void main(String[] args) {
-		System.out.print("Dzisiaj zaczynam kurs");
-		System.out.print("języka JAVA");
+		System.out.print("Dzisiaj zaczynam kurs"+System.lineSeparator());
+		System.out.print("języka JAVA"+System.lineSeparator());
 		System.out.print("w CodersLab");
 
 	}
